1087f619c2652eb9ac59c2309ed1e32d10bcf0d0
mapogolions <ivanovpavelalex45@gmail.com>
2020-07-25 18:06:08 +0500

    remove old dir

--- spliterator-learn/src/main/java/io/github/mapogolions/EntryPoint.java
+++
@@ -1,9 +0,0 @@
-package io.github.mapogolions;
-
-
-public class EntryPoint {
-    public static void main(String[] args) {
-        var someone = new Person("Some", "One");
-        System.out.println(someone);
-    }
-}
