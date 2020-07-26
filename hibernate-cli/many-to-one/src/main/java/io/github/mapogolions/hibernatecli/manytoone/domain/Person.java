008cf8e71bc065f82b872937be6f8cc5a7001f62
mapogolions <ivanovpavelalex45@gmail.com>
2020-07-26 13:20:36 +0500

    fix foreign key

--- hibernate-cli/many-to-one/src/main/java/io/github/mapogolions/hibernatecli/manytoone/domain/Person.java
+++ hibernate-cli/many-to-one/src/main/java/io/github/mapogolions/hibernatecli/manytoone/domain/Person.java
@@ -12,6 +12,6 @@ public class Person {
     private String name;
 
     @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(foreignKey = @ForeignKey(name = "student_permanent_residence_fkey"))
+    @JoinColumn(foreignKey = @ForeignKey(name = "person_permanent_residence_fkey"))
     private Address permanentResidence;
 }
