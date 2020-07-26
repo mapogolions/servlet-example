1a908a318de23dc952e1fbc1aabc5f8776d6f76f
mapogolions <ivanovpavelalex45@gmail.com>
2020-07-26 11:08:16 +0500

    structural version

--- hibernate-cli/basics/src/main/java/io/github/mapogolions/hibernatecli/basics/Samples.java
+++ hibernate-cli/basics/src/main/java/io/github/mapogolions/hibernatecli/basics/Samples.java
@@ -1,157 +1,198 @@
 package io.github.mapogolions.hibernatecli.basics;
 
 import io.github.mapogolions.hibernatecli.basics.domain.*;
+
 import java.time.LocalDate;
+import java.util.function.Consumer;
+
 import static io.github.mapogolions.hibernatecli.jpacore.JpaCore.*;
 
 public class Samples {
     public static void getReferenceOnlyCreatesProxyObject() {
-        db(
-                atomicBlock(em -> em.getReference(Hero.class, 1L))
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(em -> em.getReference(Hero.class, 1L))
+            )
+        );
     }
 
     public static void nothingToCommitWhenPersistenceContextIsEmpty() {
-        db(
-                atomicBlock(
-                        em -> em.persist(new Person("Some", "One", Gender.MALE,
-                                "someone@gmail.com", LocalDate.of(2020, 02, 15), "Columbia")),
-                        em -> em.persist(new Person("John", "Doe", Gender.MALE,
-                                "johndoe@gmail.com", LocalDate.of(2020, 02, 15), "USA")),
-                        em -> em.clear()
-                )
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(
+                            em -> em.persist(new Person("Some", "One", Gender.MALE,
+                                    "someone@gmail.com", LocalDate.of(2020, 02, 15), "Columbia")),
+                            em -> em.persist(new Person("John", "Doe", Gender.MALE,
+                                    "johndoe@gmail.com", LocalDate.of(2020, 02, 15), "USA")),
+                            em -> em.clear()
+                    )
+            )
+        );
     }
 
     public static void detachRemoveObjectFromPersistenceContext() {
-        db(
-                atomicBlock(
-                        em -> em.persist(new Book("1a", "who")),
-                        em -> em.detach(em.find(Book.class, "1a"))
-                )
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(
+                            em -> em.persist(new Book("1a", "who")),
+                            em -> em.detach(em.find(Book.class, "1a"))
+                    )
+            )
+        );
     }
 
     public static void removeTransientObjectFromFirstLevelCacheDoesNotPreventInsertion() {
-        db(
-                atomicBlock(
-                        em -> em.persist(new Book("1a", "who")),
-                        em -> em.remove(em.find(Book.class, "1a"))
-                )
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(
+                            em -> em.persist(new Book("1a", "who")),
+                            em -> em.remove(em.find(Book.class, "1a"))
+                    )
+            )
+        );
     }
 
     public static void removeTransientObjectWithNaturalKeyExecutesSelectQuery() {
-        db(
-                atomicBlock(em -> em.remove(new Book("1a", "who")))
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(em -> em.remove(new Book("1a", "who")))
+            )
+        );
     }
 
     public static void removeTransientObjectWithSurrogateKeyDoesNothing() {
-        db(
-                atomicBlock(em -> em.remove(new Hero("true hero")))
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(em -> em.remove(new Hero("true hero")))
+            )
+        );
     }
 
     public static void forceExecuteDelayedWriteOperation() {
-        db(
-                atomicBlock(
-                        em -> em.persist(new Person("Some", "One", Gender.MALE,
-                                "someone@gmail.com", LocalDate.of(2020, 02, 15), "Columbia")),
-                        em -> em.createNativeQuery("select * from person").getResultList(),
-                        em -> em.persist(new Person("John", "Doe", Gender.MALE,
-                                "johndoe@gmail.com", LocalDate.of(2020, 02, 15), "USA")),
-                        em -> em.createNativeQuery("select * from person").getResultList()
-                )
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(
+                            em -> em.persist(new Person("Some", "One", Gender.MALE,
+                                    "someone@gmail.com", LocalDate.of(2020, 02, 15), "Columbia")),
+                            em -> em.createNativeQuery("select * from person").getResultList(),
+                            em -> em.persist(new Person("John", "Doe", Gender.MALE,
+                                    "johndoe@gmail.com", LocalDate.of(2020, 02, 15), "USA")),
+                            em -> em.createNativeQuery("select * from person").getResultList()
+                    )
+            )
+        );
     }
 
     public static void eachSessionHasItsOwnFirstLevelCache() {
-        db(
-                session(
-                        atomicBlock(em -> em.persist(new Hero("true hero"))),
-                        atomicBlock(em -> em.find(Hero.class, 1L))
-                ),
-                session(
-                        atomicBlock(em -> em.find(Hero.class, 1L))
-                )
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    session(
+                            atomicBlock(em -> em.persist(new Hero("true hero"))),
+                            atomicBlock(em -> em.find(Hero.class, 1L))
+                    ),
+                    session(
+                            atomicBlock(em -> em.find(Hero.class, 1L))
+                    )
+            )
+        );
     }
 
     public static void firstLevelCacheHasCrossTransactionNature() {
-        db(
-                session(
-                        atomicBlock(em -> em.persist(new Hero("true hero"))),
-                        atomicBlock(em -> em.find(Hero.class, 1L))
-                )
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    session(
+                            atomicBlock(em -> em.persist(new Hero("true hero"))),
+                            atomicBlock(em -> em.find(Hero.class, 1L))
+                    )
+            )
+        );
     }
 
     public static void retrievalsFetchEntitiesFromDatabaseIfFirstLevelCacheDoesNotContainEntity() {
-        db(
-                atomicBlock(em -> em.find(Hero.class, 1L))
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(em -> em.find(Hero.class, 1L))
+            )
+        );
     }
 
     public static void retrievalsFirstOfAllSearchForEntitiesInFirstLevelCache() {
-        db(
-                atomicBlock(
-                        em -> em.persist(new Hero("true hero")),
-                        em -> em.find(Hero.class, 1L)
-                )
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(
+                            em -> em.persist(new Hero("true hero")),
+                            em -> em.find(Hero.class, 1L)
+                    )
+            )
+        );
     }
 
     public static void sequenceGenerationStrategyDelayActualInsertionAsLongAsPossible() {
-        db(
-                atomicBlock(
-                        em -> em.persist(new Person("Some", "One", Gender.MALE,
-                                "someone@gmail.com", LocalDate.of(2020, 02, 15), "Columbia")),
-                        em -> em.persist(new Person("John", "Doe", Gender.MALE,
-                                "johndoe@gmail.com", LocalDate.of(2020, 02, 15), "USA"))
-                )
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(
+                            em -> em.persist(new Person("Some", "One", Gender.MALE,
+                                    "someone@gmail.com", LocalDate.of(2020, 02, 15), "Columbia")),
+                            em -> em.persist(new Person("John", "Doe", Gender.MALE,
+                                    "johndoe@gmail.com", LocalDate.of(2020, 02, 15), "USA"))
+                    )
+            )
+        );
     }
 
     public static void identityGenerationStrategyInsertsRecordsImmediately() {
-        db(
-                atomicBlock(
-                        em -> em.persist(new Hero("Togo")),
-                        em -> em.persist(new Hero("Balto"))
-                )
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(
+                            em -> em.persist(new Hero("Togo")),
+                            em -> em.persist(new Hero("Balto"))
+                    )
+            )
+        );
     }
 
     public static void mergeUpdatesOnlyWhenNecessary() {
-        db(
-                atomicBlock(
-                        em -> em.persist(new Book("1a-2b-3c", "Someone")),
-                        em -> em.merge(new Book("1a-2b-3c", "Someone"))
-                )
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(
+                            em -> em.persist(new Book("1a-2b-3c", "Someone")),
+                            em -> em.merge(new Book("1a-2b-3c", "Someone"))
+                    )
+            )
+        );
     }
 
     public static void mergeIsRoundTripOperationWhenNaturalPrimaryKeyIsUsed() {
-        db(
-                atomicBlock(em -> em.merge(new Book("1a-2b-3c", "Someone")))
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(em -> em.merge(new Book("1a-2b-3c", "Someone")))
+            )
+        );
     }
 
     public static void mergeHasOnlyInsertionSemanticWhenSurrogatePrimaryKeyIsUsed() {
-        db(
-                atomicBlock(em -> em.merge(new Hero("Togo")))
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(em -> em.merge(new Hero("Togo")))
+            )
+        );
     }
 
     public static void trackingManagedRecordState() {
-        db(
-                atomicBlock(
-                        em -> {
-                            var someone = new Person("Some", "One", Gender.MALE,
-                                    "someone@yandex.ru", LocalDate.of(2020, 02, 15), "Columbia");
-                            em.persist(someone);
-                            someone.email("someone@gmail.com");
-                        })
-        ).accept("hibernate-cli.basics");
+        basicPersistenceUnit(
+            db(
+                    atomicBlock(
+                            em -> {
+                                var someone = new Person("Some", "One", Gender.MALE,
+                                        "someone@yandex.ru", LocalDate.of(2020, 02, 15), "Columbia");
+                                em.persist(someone);
+                                someone.email("someone@gmail.com");
+                            })
+            )
+        );
+    }
+
+    public static void basicPersistenceUnit(Consumer<String> fn) {
+        fn.accept("hibernate-cli.basics");
     }
 }
